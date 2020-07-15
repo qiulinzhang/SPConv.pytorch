78c734f61e0b9bd5146239db4a668fcd7c45a4c8
Qiulin Zhang <1242526239@qq.com>
2020-07-15 14:19:47 +0800

    fix basicblock for res18 and res34

--- code/resnet_sp.py
+++ code/resnet_sp.py
@@ -80,10 +80,12 @@ class BasicBlock(nn.Module):
         if dilation > 1:
             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
+        #self.conv1 = conv3x3(inplanes, planes, stride)
+        self.conv1 = SPConv_3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
+        # self.conv2 = conv3x3(planes, planes)
+        self.conv2 = SPConv_3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
